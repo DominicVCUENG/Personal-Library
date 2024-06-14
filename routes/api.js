ac63f1044ad7f6ab0231b7287933b1b18bd3592b
Dominic Mason <masond4@vcu.edu>
2024-06-14 09:05:39 -0400

    satisfying all reqs

--- routes/api.js
+++ routes/api.js
@@ -8,40 +8,118 @@
 
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
+
+// In-memory storage for books
+let books = {};
+
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      // Retrieve all books
+      const bookArray = Object.values(books).map(book => ({
+        _id: book._id,
+        title: book.title,
+        commentcount: book.comments.length
+      }));
+      res.json(bookArray);
     })
     
     .post(function (req, res){
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+
+      // Check if title is provided
+      if (!title) {
+        res.send('missing required field title');
+        return;
+      }
+
+      // Create a new book
+      const newBook = {
+        _id: uuidv4(),
+        title: title,
+        comments: []
+      };
+
+      // Store the book in memory
+      books[newBook._id] = newBook;
+
+      // Respond with the new book object
+      res.json({ _id: newBook._id, title: newBook.title });
     })
     
     .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+      // Clear all books from memory
+      books = {};
+      res.send('complete delete successful');
     });
-
-
 
   app.route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+
+      // Find the book by ID
+      const book = books[bookid];
+
+      // If book not found, respond with appropriate message
+      if (!book) {
+        res.send('no book exists');
+        return;
+      }
+
+      // Respond with the book object including comments
+      res.json({
+        _id: book._id,
+        title: book.title,
+        comments: book.comments
+      });
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
-      //json res format same as .get
+
+      // Check if comment is provided
+      if (!comment) {
+        res.send('missing required field comment');
+        return;
+      }
+
+      // Find the book by ID
+      const book = books[bookid];
+
+      // If book not found, respond with appropriate message
+      if (!book) {
+        res.send('no book exists');
+        return;
+      }
+
+      // Add the comment to the book
+      book.comments.push(comment);
+
+      // Respond with the updated book object
+      res.json({
+        _id: book._id,
+        title: book.title,
+        comments: book.comments
+      });
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+
+      // Check if book exists
+      if (!books[bookid]) {
+        res.send('no book exists');
+        return;
+      }
+
+      // Delete the book
+      delete books[bookid];
+
+      // Respond with success message
+      res.send('delete successful');
     });
   
 };
